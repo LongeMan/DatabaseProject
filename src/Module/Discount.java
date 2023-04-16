0d8d75cce70cd9534c7e6e7df30b1c27f065f72b
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-04-16 16:37:04 +0200

    Create Discount.java

---
+++ src/Module/Discount.java
@@ -0,0 +1,14 @@
+package Module;
+import Controller.*;
+
+public class Discount {
+    private String pID;
+    private double discount;
+    private String period;
+    private String dCode;
+
+    public void addDiscount(String pID, double discount, String period, String dCode){
+
+    }
+
+}

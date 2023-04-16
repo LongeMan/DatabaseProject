e9e3f3189ff0c46d2be3669d14ff6df137eb279a
Oliver Bengtsson <58479844+LongeMan@users.noreply.github.com>
2023-04-16 16:27:06 +0200

    Added functions to product

--- src/Module/Product.java
+++ src/Module/Product.java
@@ -1,4 +1,9 @@
 package Module;
+import Controller.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Product {
 
@@ -6,30 +11,69 @@ public class Product {
     private String pID;
     private int pQuantity;
     private int pBasePrice;
-    private String pSuplier;
+    private String pSupplier;
 
     /**
-     * Constructor for product
-     * @param pName
-     * @param pID
-     * @param pQuantity
-     * @param pBasePrice
-     * @param pSuplier
+     * Decreases the amount of products for a specific product
+     * @param pID Product ID
+     * @param decreasingQuantity amount to decrease for the product
      */
-
-    public Product(String pName, String pID, int pQuantity, int pBasePrice, String pSuplier){
-        this.pName = pName;
-        this.pID = pID;
-        this.pQuantity = pQuantity;
-        this.pBasePrice = pBasePrice;
-        this.pSuplier = pSuplier;
-    }
-
     public void decreaseQuantity(String pID, int decreasingQuantity){
         pQuantity = pQuantity - decreasingQuantity;
     }
+
+    /**
+     * Increases the amount of products for a specific product
+     * @param pID Product ID
+     * @param increaseQuantity amount to increase the quantity for the product
+     */
     public void increaseQuantity(String pID, int increaseQuantity){
         pQuantity = pQuantity + increaseQuantity;
+    }
+
+    /**
+     * Adds a product to the SQL database
+     * @param pName Product name
+     * @param pID Product ID
+     * @param pQuantity Product quantity
+     * @param pBasePrice Products baseprice
+     * @param pSupplier Products supplier
+     */
+
+    public void addProduct(String pName, String pID, int pQuantity, int pBasePrice, String pSupplier){
+        try (Connection con = Main.getDatabase()){
+            String sql = "INSERT INTO PRODUCT (p_name, p_id, p_quantity, p_baseprice, p_supplier) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1, pName);
+            stmt.setString(2, pID);
+            stmt.setInt(3, pQuantity);
+            stmt.setInt(4, pBasePrice);
+            stmt.setString(5, pSupplier);
+
+            stmt.executeUpdate();
+            System.out.println("Record inserted successfully");
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Removes a product from the database
+     * @param pID product ID which is designated
+     */
+    public void removeProduct(String pID){
+        try(Connection con = Main.getDatabase()){
+            String sql = (" DELETE FROM PRODUCT WHERE p_id = ?");
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setString(1,pID);
+            stmt.executeUpdate();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
 }
